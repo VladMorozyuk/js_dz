e1b1a83f2ce1c78ce6d1d037a2ce8c5a2e684d0f
VladMorozyuk <moroziukvladyslav@gmail.com>
2025-12-01 13:29:12 +0200

    file js dz 19

---
+++ greetByName.js
@@ -0,0 +1,8 @@
+function greetByName(msg, name) {
+  return msg + ", " + name;
+}
+
+
+console.log(greetByName('Hi', 'John'));   
+console.log(greetByName('Hey', 'Bob'));   
+console.log(greetByName('Hello', 'Mary'));
---
+++ sumBigIntegers.js
@@ -0,0 +1,6 @@
+function sumBigIntegers(numStr1, numStr2) {
+  return BigInt(numStr1) + BigInt(numStr2);
+}
+
+
+console.log(sumBigIntegers('9007199254740991', '9007199254740991'));
